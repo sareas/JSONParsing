5e8fad55e047df66c2631a81662fa7dc913095d4
sareas <sabrinareas@gmail.com>
2015-11-11 11:27:31 -0500

    initial commit

---
+++ JSONParsing/JSONParsing/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JSONParsing
+//
+//  Created by Sabrina Arêas on 2015-11-11.
+//  Copyright © 2015 Open Closet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JSONParsing/JSONParsing/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JSONParsing
+//
+//  Created by Sabrina Arêas on 2015-11-11.
+//  Copyright © 2015 Open Closet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
